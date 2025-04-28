7575aa0921cd0a924b6e974909023b15543001c8
Krishna <krishnagovarthini-lmnas>
2025-04-29 00:10:34 +0530

    Strapi integrated

---
+++ app/api/getData.ts
@@ -0,0 +1,40 @@
+import { ITransformer } from '@repo/middleware';
+import { unstable_cache } from 'next/cache';
+
+export type Tcontext = {
+    locale: string
+    filters?: {
+        slug: {
+            eq: string
+        }
+    }
+}
+const LdCacheMap = new Map<string, ReturnType<typeof unstable_cache>>();
+
+export async function fnGetCacheData<DynamicSourceType, DynamicTargetType>(
+    iContext: Tcontext,
+    transformer: ITransformer<DynamicSourceType, DynamicTargetType>
+) {
+    const locale = iContext?.locale ?? 'en';
+
+    let slug;
+    if (iContext?.filters?.slug?.eq) {
+        slug = iContext.filters.slug.eq;
+    }
+
+    // const LCacheKey = `${transformer.contentType}-${locale}-${slug}`;
+    const LCacheKey = slug ? `${transformer.contentType}-${locale}-${slug}` : `${transformer.contentType}-${locale}`;
+    if (!LdCacheMap.has(LCacheKey)) {
+        const fetcher = unstable_cache(
+            async () => {
+                const pageData: DynamicTargetType = await transformer.execute(iContext);
+                return pageData;
+            },
+            [LCacheKey],
+            { revalidate: 2, tags: slug ? [LCacheKey, locale, slug] : [LCacheKey, locale] }
+        );
+        LdCacheMap.set(LCacheKey, fetcher);
+    }
+
+    return await LdCacheMap.get(LCacheKey)!();
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,39 @@
+import { Tcontext } from "@repo/middleware";
 import Footer from "@repo/ui/components/footer";
 import Navbar from "@repo/ui/components/navbar";
 import TitleSubtitle from "@repo/ui/components/titleSubtitle";
+import { TfooterTarget } from "../../../packages/middleware/src/types";
+import { clTransformerFactory } from "@repo/middleware";
+import { fnGetCacheData } from "./api/getData";
 
 const HeroData = {
-  heading:{
+  heading: {
     textWithoutColor: "Blog Coming Soon...",
-    subtitle:"Stay Tuned for Exciting Updates!"
+    subtitle: "Stay Tuned for Exciting Updates!"
   }
 }
-export default function Blog() {
+export default async function Blog({
+  params,
+}: {
+  params: Promise<{
+    locale: string;
+  }>;
+}) {
+  const { locale } = await params;
+  const context: Tcontext = { locale: locale }
+
+  const footerData: TfooterTarget = await fnGetCacheData(
+    context,
+    clTransformerFactory.createTransformer('footer')
+  );
   return (
     <div >
       <Navbar />
       <section className="border-b border-border/40 py-20 bg-gradient-to-b from-primary to-primary/70 text-background">
-      <div className="container mx-auto px-4 md:px-6">
-      <TitleSubtitle idTitle={HeroData.heading} />
-      </div>
+        <div className="container mx-auto px-4 md:px-6">
+          <TitleSubtitle idTitle={HeroData.heading} />
+        </div>
       </section>
-      <Footer />
-    </div>
-  )
+      <Footer idFooter={footerData} />
+    </div>);
 }
-
