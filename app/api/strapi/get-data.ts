5a57664044c3ef5a9937e3e99fcc5980d72bd491
Krishna <krishnagovarthini-lmnas>
2026-02-27 15:10:49 +0530

    get-data.ts

--- app/api/strapi/get-data.ts
+++ app/api/strapi/get-data.ts
@@ -1,14 +1,6 @@
 import { unstable_cache } from "next/cache"
-import { ITransformer } from "@repo/middleware/types"
+import { ITransformer, Tcontext } from "@repo/middleware/types"
 
-export type Tcontext = {
-  locale: string
-  filters?: {
-    slug: {
-      eq: string
-    }
-  }
-}
 const LdCacheMap = new Map<string, ReturnType<typeof unstable_cache>>()
 
 export async function fnGetCacheData<DynamicSourceType, DynamicTargetType>(
